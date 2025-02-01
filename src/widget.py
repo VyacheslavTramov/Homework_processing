7f1c390bb9a14a80b86218f70ee04698026b401b
Вячеслав Трамов <tram7070@yandex.ru>
2025-02-01 18:34:14 +0300

    .

---
+++ src/widget.py
@@ -0,0 +1,9 @@
+
+def mask_account_card(account_card: str | int) -> str:
+    '''Разделяем принимаемую строку на отдельные элементы'''
+    inp_list = [elem for elem in account_card.split()]
+
+    for l in list:
+        list_len = len(inp_list)
+
+
