a04750de9236f743217cb605e60d28f2ac1f52a0
Вячеслав Трамов <tram7070@yandex.ru>
2025-02-03 14:16:10 +0300

    Dorabotka DZ

--- src/widget.py
+++ src/widget.py
@@ -1,38 +1,37 @@
-from masks import *
+from masks import get_mask_account, get_mask_card_number
 
 input_card = input()
+
+
 def mask_account_card(card: str | int) -> str:
-    '''Разделяем принимаемую строку на отдельные элементы'''
+    """Разделяем принимаемую строку на отдельные элементы"""
     card_split = [elem for elem in card.split()]
 
-    number = card_split [-1]
+    number = card_split[-1] # извлекаем последний элемент списка
 
-    if len (card_split) == 2:
-        if len(number) == 16:
-            new_number = get_mask_card_number(number)
+    if len(card_split) == 2: # редполагается, что карта состоит только из типа карты и её номера
+        if len(number) == 16: # если номер карты равен 16-ти числам
+            new_number = get_mask_card_number(number) # импорт функции из masks.py
         else:
-            new_number = get_mask_account(number)
-        card_connect = f'{card_split[0]} {new_number}'
+            new_number = get_mask_account(number) # импорт функции из masks.py
+        card_connect = f"{card_split[0]} {new_number}" # соединение типа карты и номера
     else:
-        new_number = get_mask_card_number(number)
-        card_connect = f'{card_split[0]} {card_split[1]} {new_number}'
+        new_number = get_mask_card_number(number) # импорт функции из masks.py
+        card_connect = f"{card_split[0]} {card_split[1]} {new_number}" # соединение типа карты и номера
 
     return card_connect
+
+
 print(mask_account_card(input_card))
 
 
-
 def get_date(number: str | int) -> str:
-
+    '''Делаем срез нужной нам части вводимой строки'''
     new_string = number[0:10]
-    to_share = [x for x in new_string.split("-")]
-    format_date = f'{to_share[-1]}.{to_share[-2]}.{to_share[0]}'
+    to_share = [x for x in new_string.split("-")] # разделяем срез на элементы
+    format_date = f"{to_share[-1]}.{to_share[-2]}.{to_share[0]}" # преобразовываем элементы в нужнй формат
+
+    return format_date
 
 
-
-
-
-
-
-
-
+print(get_date("2024-03-11T02:26:18.671407"))
