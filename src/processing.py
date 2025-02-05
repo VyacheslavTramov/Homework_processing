2c756a44f10f1015730ed5a5668342efc916d794
Вячеслав Трамов <tram7070@yandex.ru>
2025-02-05 11:01:08 +0300

    DZ 05/02

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-lst = [
+date = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
@@ -7,16 +7,22 @@ lst = [
 
 
 def filter_by_state(lst_: list) -> list:
-    '''Проходим циклом по словарю и по ключу "по умолчанию" создаем новый список'''
+    """Проходим циклом по словарю"""
     new_list = []
     for dict in lst_:
-
+        # по ключу "по умолчанию" создаем новый список
         key_list = dict.get("state")
         if key_list == "EXECUTED":
             new_list.append(dict)
     return new_list
 
 
-print(filter_by_state(lst))
+print(filter_by_state(date))
 
 
+def sort_by_date(sort_date: list[dict], reverse: bool = True) -> list[dict]:
+    '''Сортируем входящий список по убыванию используя ключ "date"'''
+    return sorted(sort_date, key=lambda x: x["date"], reverse=reverse)
+
+
+print(sort_by_date(date))
